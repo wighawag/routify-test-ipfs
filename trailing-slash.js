9a473a003239c5bf91c918a313c197623dd96cea
Wighawag <wighawag@gmail.com>
2020-05-23 08:51:19 +0100

    fix ipfs emulation

--- trailing-slash.js
+++ trailing-slash.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const EventEmitter = require('events')
 
 class Redirect extends EventEmitter {
@@ -12,14 +13,30 @@ class Redirect extends EventEmitter {
   middleware (config) {
     return async function (ctx, next) {
         let redirectToUrl = ctx.request.href;
-        const length = redirectToUrl.length;
-        if (length == 0) {
-            redirectToUrl = "/"
-        } else if (redirectToUrl[length - 1] != "/") {
-            redirectToUrl += "/";
+        const doubleSLashIndex = redirectToUrl.indexOf("//");
+        let urlWithoutProtocol;
+        if (doubleSLashIndex >= 0) {
+          urlWithoutProtocol = redirectToUrl.slice(doubleSLashIndex+2);
+        } else {
+          urlWithoutProtocol = redirectToUrl;
         }
-        if (redirectToUrl !== ctx.request.href) {
-            ctx.redirect(redirectToUrl)
+        const firsSlashIndex = urlWithoutProtocol.indexOf("/");
+        if (firsSlashIndex !== -1) {
+          const path = urlWithoutProtocol.slice(firsSlashIndex+1);
+          if (fs.existsSync(path)) {
+            const stats = fs.statSync(path);
+            if (stats.isDirectory()) {
+              const length = redirectToUrl.length;
+              if (length == 0) {
+                  redirectToUrl = "/"
+              } else if (redirectToUrl[length - 1] != "/") {
+                  redirectToUrl += "/";
+              }
+              if (redirectToUrl !== ctx.request.href) {
+                  ctx.redirect(redirectToUrl)
+              }
+            }
+          }
         }
         await next()
         }
